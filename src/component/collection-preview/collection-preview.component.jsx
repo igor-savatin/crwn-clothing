6bdc3641cf15785d9f94919cb7ebb741c2025932
unknown <igoreduardosavatin@gmail.com>
2020-03-27 19:55:12 -0300

    inicio da página de items

--- src/component/collection-preview/collection-preview.component.jsx
+++ src/component/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import ColletionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 classname='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.map(item =>(
-                <div key={item.id}>{item.name}</div>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(({ id, ...otherItemProps }) =>(
+                    <ColletionItem key={id} { ...otherItemProps}/>
             ))}
         </div>
     </div>
